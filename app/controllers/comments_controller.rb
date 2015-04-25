f6f7c5f3aa3401bc62291fa65389b4bfbfb86941
Konrad Szczesniak <konrad.szczesniak@elpassion.pl>
2015-04-25 13:03:34 +0200

    adding comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 class CommentsController < ApplicationController
   def create
+  	book = Book.find(params[:book_id])
+  	book.comments.create(permitted_params)
+  	redirect_to book
+  end
+
+  def permitted_params
+  	params.require(:comment).permit(:content)
   end
 end
