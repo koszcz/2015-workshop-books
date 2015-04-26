5f000b74a12846c48d4e12da1b3ec818d4fac528
Konrad Szczesniak <konrad.szczesniak@elpassion.pl>
2015-04-26 11:20:24 +0200

    comment made_within_one_hour method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   include Likeable
   
   belongs_to :book
+
+  def made_within_one_hour?(current_time)
+  	current_time - created_at <= 1.hour
+  end
 end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe Comment, type: :model do
+
+  describe '#made_within_one_hour?' do
+
+    before(:each) do
+      @comment = Comment.new
+      @current_time = Time.new(2014, 1, 1, 12,  0, 0)
+    end
+
+    it 'should return true when comment is added within an hour' do
+      comment_time = Time.new(2014, 1, 1, 11, 30, 0)
+      allow(@comment).to receive(:created_at).and_return comment_time
+
+      expect(@comment.made_within_one_hour?(@current_time)).to eq(true)
+    end
+
+    it 'should return false when comment is added over an hour ago' do
+      comment_time = Time.new(2014, 1, 1, 10, 0, 0)
+      allow(@comment).to receive(:created_at).and_return comment_time
+
+      expect(@comment.made_within_one_hour?(@current_time)).to eq(false)      
+    end
+  end
+
+end	
